44a27f66564fb23032d79e0ce991578ee75dd5aa
phelipe <phelipe.pereira@projuris.com.br>
2020-09-21 11:21:09 -0300

    exercicios

---
+++ exerNivel1/exer1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+/*
+- Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+    1. 42
+    2. "James Bond"
+    3. true
+- Agora demonstre os valores nestas variáveis, com:
+    1. Uma única declaração print
+    2. Múltiplas declarações print
+*/
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println("Hello, playground")
+
+	fmt.Printf("x: %v, y: %v, z: %v", x, y, z)
+	fmt.Printf("\nx: %v", x)
+	fmt.Printf("\ny: %v, z: %v", y)
+	fmt.Printf("\nz: %v", z)
+}
---
+++ exerNivel1/exer4.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+/*
+- Crie um tipo. O tipo subjacente deve ser int.
+- Crie uma variável para este tipo, com o identificador "x", utilizando a palavra-chave var.
+- Na função main:
+    1. Demonstre o valor da variável "x"
+    2. Demonstre o tipo da variável "x"
+    3. Atribua 42 à variável "x" utilizando o operador "="
+    4. Demonstre o valor da variável "x"
+*/
+
+type novoTipo int
+var x novoTipo
+
+func main() {
+	fmt.Printf("x: %v, Tipo: %T\n", x, x)
+	x = 42
+	fmt.Printf("x: %v", x)
+
+}
