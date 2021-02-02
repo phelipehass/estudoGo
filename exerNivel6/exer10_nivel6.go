2facd2b2a5eadc5350f60dfb707d2b866c4b0376
phelipe <phelipe.pereira@projuris.com.br>
2021-02-02 12:34:00 -0300

    exercícios nivel 6

--- exerNivel6/exer10_nivel6.go
+++ exerNivel6/exer10_nivel6.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+//- Demonstre o funcionamento de um closure.
+//- Ou seja: crie uma função que retorna outra função, onde esta outra função faz uso de uma variável alem de seu scope.
+
+func main() {
+	a := contPlus()
+	b := contPlus()
+
+	fmt.Println(a()) //1
+	fmt.Println(a()) //2
+	fmt.Println(a()) //3
+	fmt.Println(b()) //1
+	fmt.Println(b()) //2
+	fmt.Println(b()) //3
+}
+
+func contPlus() func() int {
+	cont := 0
+	return func() int {
+		cont += 1
+		return cont
+	}
+}
