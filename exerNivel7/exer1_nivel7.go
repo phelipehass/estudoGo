0cdf8098236eb61446804a2f110c2e8cb4a09d75
phelipe <phelipe.pereira@projuris.com.br>
2021-02-02 13:28:38 -0300

    exercícios nivel 7

---
+++ exerNivel7/exer1_nivel7.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+//- Crie um valor e atribua-o a uma variável.
+//- Demonstre o endereço deste valor na memória.
+
+func main() {
+	a := 7
+	fmt.Println(&a)
+}
---
+++ exerNivel7/exer2_nivel7.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+//- Crie um struct "pessoa"
+//- Crie uma função chamada mudeMe que tenha *pessoa como parâmetro. Essa função deve mudar um valor armazenado no endereço *pessoa.
+//- Dica: a maneira "correta" para fazer dereference de um valor em um struct seria (*valor).campo
+//- Mas consta uma exceção na documentação. Link: https://golang.org/ref/spec#Selectors​
+//- "As an exception, if the type of x is a named pointer type and (*x).f is a valid selector expression denoting a field (but not a method),
+//- → x.f is shorthand for (*x).f." ←
+//- Ou seja, podemos usar tanto o atalho p1.nome quanto o tradicional (*p1).nome
+//- Crie um valor do tipo pessoa;
+//- Use a função mudeMe e demonstre o resultado.
+
+type person struct {
+	name     string
+	lastName string
+}
+
+func mudeMe(p *person) {
+	(*p).lastName = "Silva"
+}
+
+func main() {
+	p := person{"Joanadabe", "Cunha"}
+	fmt.Println(p.name, p.lastName)
+
+	mudeMe(&p)
+
+	fmt.Println(p.name, p.lastName)
+}
