c6f04af102fbcb68399e0a6f10e94c004de0d362
phelipe <phelipe.pereira@projuris.com.br>
2021-04-07 12:51:04 -0300

    Término exercício 7 nível 10.

--- exerNivel10/exer7_nivel10.go
+++ exerNivel10/exer7_nivel10.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
 //- Crie um programa que lance 10 goroutines onde cada uma envia 10 números a um canal;
 //- Tire estes números do canal e demonstre-os.
 
 func main() {
-	maxGoRoutines := 10
+	c := make(chan int)
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 10; j++ {
+				c <- j
+			}
+		}()
+	}
 
+	for v := 0; v < 100; v++ {
+		fmt.Println(v, " - ", <-c)
+	}
 }
