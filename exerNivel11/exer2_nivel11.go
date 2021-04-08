13b9c3d8332cbdea78d0fe46f12d4ba3232a3b14
phelipe <phelipe.pereira@projuris.com.br>
2021-04-08 13:29:08 -0300

    Início dos exercícios nível 11.

---
+++ exerNivel11/exer2_nivel11.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+//- Utilizando este código: https://play.golang.org/p/9a1IAWy5E6
+//- ...crie uma mensagem de erro customizada utilizando fmt.Errorf().
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(bs))
+
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error in json marshal: %q", err)
+	}
+
+	return bs, nil
+}
